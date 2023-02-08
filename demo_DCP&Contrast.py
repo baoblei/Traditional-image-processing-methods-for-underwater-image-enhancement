833d0581e0e5d12f9e0f29522079c2a919eea1d6
baobinglei <baobinglei@foxmail.com>
2023-02-08 21:59:27 +0800

    local_hist

--- demo_DCP&Contrast.py
+++ demo_DCP&Contrast.py
@@ -4,11 +4,11 @@ import numpy as np
 # import time 
 
 # 新建一个VideoCapture对象，打开视频
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('./video/uw4.mp4')
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('./video/uw4.mp4')
 
 # 获取原始视频帧大小及fps信息，注意宽高需要强制类型转换成int，否则VideoWriter会报错
-width = int(cap.get(3))
+width = int(cap.get(3)/2)
 height = int(cap.get(4))
 fps = 24
 # 根据视频文件属性设置waitTime和fps
@@ -25,7 +25,7 @@ frame2gray = np.zeros((height, width, 3), np.uint8)
 # times = []
 while 1:
     ret, frame = cap.read()
-    # frame = frame[:,:320]
+    frame = frame[:,:640]
     if frame is None:
         break
     else:
